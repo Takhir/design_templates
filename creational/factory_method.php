282f33a9fff6f3e288ae75c78d6411c6dd8d947b
Takhir Salimov <takhir.salimov@almatv.kz>
2024-02-13 18:22:12 +0600

    Initial commit

---
+++ creational/factory_method.php
@@ -0,0 +1,50 @@
+<?php
+//
+//interface Worker
+//{
+//    public function work();
+//}
+//class Developer implements Worker
+//{
+//    public function work()
+//    {
+//        printf('in developing');
+//    }
+//}
+//
+//class Designer implements Worker
+//{
+//    public function work()
+//    {
+//        printf('in designing');
+//    }
+//}
+//
+//interface WorkerFactory
+//{
+//    public static function make();
+//}
+//
+//class DeveloperFactory implements WorkerFactory
+//{
+//
+//    public static function make()
+//    {
+//        return new Developer();
+//    }
+//}
+//
+//
+//class DesignerFactory implements WorkerFactory
+//{
+//    public static function make()
+//    {
+//        return new Designer();
+//    }
+//
+//}
+//
+//$developer = DeveloperFactory::make();
+//$designer = DesignerFactory::make();
+//
+//$developer->work();
---
+++ creational/static_factory.php
@@ -0,0 +1,39 @@
+<?php
+//interface Worker
+//{
+//    public  function work();
+//}
+//
+//class Developer implements Worker
+//{
+//    public function work()
+//    {
+//        printf('im developing');
+//    }
+//}
+//
+//class Designer implements Worker
+//{
+//    public function work()
+//    {
+//        printf('im designing');
+//    }
+//}
+//
+//class WorkerFactory
+//{
+//    public static function make($workerTitle): ?Worker
+//    {
+//        $className = strtoupper($workerTitle);
+//
+//        if (class_exists($className)) {
+//            return new $className();
+//        }
+//
+//        return null;
+//    }
+//}
+//
+//$developer = WorkerFactory::make('developer');
+//
+//$developer->work();
