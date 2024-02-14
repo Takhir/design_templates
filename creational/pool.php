8cc754df89240adf517d8ef9f4e8a755315bb99a
Takhir Salimov <takhir.salimov@almatv.kz>
2024-02-14 11:56:28 +0600

    creational

---
+++ creational/pool.php
@@ -0,0 +1,78 @@
+<?php
+
+class WorkerPoll
+{
+    private array $freeWorkers = [];
+    private array $busyWorkers = [];
+
+    /**
+     * @return array
+     */
+    public function getFreeWorkers(): array
+    {
+        return $this->freeWorkers;
+    }
+
+    /**
+     * @param array $freeWorkers
+     */
+    public function setFreeWorkers(array $freeWorkers): void
+    {
+        $this->freeWorkers = $freeWorkers;
+    }
+
+    /**
+     * @return array
+     */
+    public function getBusyWorkers(): array
+    {
+        return $this->busyWorkers;
+    }
+
+    /**
+     * @param array $busyWorkers
+     */
+    public function setBusyWorkers(array $busyWorkers): void
+    {
+        $this->busyWorkers = $busyWorkers;
+    }
+
+    public function getWorker(): Worker
+    {
+        if (count($this->freeWorkers) === 0) {
+            $worker = new Worker();
+        } else {
+            $worker = array_pop($this->freeWorkers);
+        }
+
+        $this->busyWorkers[spl_object_hash($worker)] = $worker;
+        return $worker;
+    }
+
+    public function release(Worker $worker)
+    {
+        $key = spl_object_hash($worker);
+
+        if(isset($this->busyWorkers[$key])) {
+            unset($this->busyWorkers[$key]);
+            $this->freeWorkers[$key] = $worker;
+        }
+    }
+}
+
+class Worker
+{
+    public function work()
+    {
+        printf('Im working');
+    }
+}
+
+$pool = new WorkerPoll();
+
+$worker = $pool->getWorker();
+$worker2 = $pool->getWorker();
+$worker3 = $pool->getWorker();
+$pool->release($worker3);
+var_dump($pool->getFreeWorkers());
+var_dump($pool->getBusyWorkers());
